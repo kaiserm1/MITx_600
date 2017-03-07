dc84d66f1d734b6a179d01999b8c3a624d46e18f
kaiserm1 <kaiserm1@gmail.com>
2017-03-07 19:31:36 +0100

    update

---
+++ Problem_Set_2_problem1.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  7 17:20:46 2017
+
+@author: maka_
+"""
+
+
+def remaining_balance(balance, annualInterestRate, monthlyPaymentRate):
+    """
+    Input: balance as float - outstanding balance on credit card
+       annualInterestRate as float - annual interest rate as a decimal
+       monthlyPaymentRate as floast - minimum monthly payment rate as a decimal
+    
+    Calculate statements on the monthly payment and remaining balance.
+    At the end of 12 months, print out the remaining balance with 2 decimal 
+    digits accuracy.
+    """
+    
+    monthly_interest_rate = annualInterestRate / 12.0
+    #minimum_monthly_payment = monthlyPaymentRate * previous_balance
+    #monthly_unpaid_balance = previous_balance - monthlyPaymentRate
+    #updated_balance_each_month = monthly_unpaid_balance + \
+    #                             monthly_interest_rate * monthly_unpaid_balance
+    
+    
+    for i in range(12):
+        monthly_payment = monthlyPaymentRate * balance
+        monthly_unpaid_balance = balance - monthly_payment
+        balance = monthly_unpaid_balance + monthly_interest_rate * monthly_unpaid_balance
+        print(balance)
+    
+    return round(balance, 2)
+
+print("Remaining balance:", remaining_balance(balance, annualInterestRate, monthlyPaymentRate))
+    
---
+++ Problem_Set_2_problem2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  7 19:29:44 2017
+
+@author: maka_
+
+Problem 2 - Paying Debt Off in a Year
+"""
+
