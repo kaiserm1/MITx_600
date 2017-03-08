cc5adf1974468ee54ab9855445d0bd70f446bf02
kaiserm1 <kaiserm1@gmail.com>
2017-03-08 14:24:06 +0100

    update

--- Problem_Set_2_problem3.py
+++ Problem_Set_2_problem3.py
@@ -15,19 +15,23 @@ def lowest_payment(balance, annualInterestRate):
     """
     
     def new_guess(lower_boundary, upper_boundary):
-        guess = (lower_boundary + upper_boundray) / 2
+        guess = (lower_boundary + upper_boundary) / 2
+        print(guess)
         return guess
     
     monthly_interest_rate = annualInterestRate / 12.0
     lower_boundary = balance / 12.0
     upper_boundary = (balance * (1 + monthly_interest_rate)**12) / 12.0
     epsilon = 0.01
+    guess = new_guess(lower_boundary, upper_boundary)
     
-    if abs(balance - (guess * 12)) <= epsilon:
-        return guess
-    else:
-        if abs(balance - (guess * 12)) > epsilon:
-            
+    while abs(balance - (guess * 12)) > epsilon:
+        if balance - (guess * 12) > epsilon:
+            new_guess(guess, upper_boundary)
+        else:
+            new_guess(lower_boundary, guess)
+    
+    return guess    
         
     
    
