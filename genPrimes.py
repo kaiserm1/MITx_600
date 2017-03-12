a2a0146546fe9a7a83e3335b92e3913a7cb4f319
Martin Kaiser <kaiserm1@gmail.com>
2017-03-12 07:04:13 +0000

    u

--- genPrimes.py
+++ genPrimes.py
@@ -1,14 +1,13 @@
-""" By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
-    that the 6th prime is 13.
-    What is the 10 001st prime number?
+""" 
+test
 """
 
 import math
 
-number = 300425737531
-final_prime = 10001
-
-# 104729
+def genPrimes():
+    primes_list = [2]
+    while True:
+        
 
 def is_prime(n):
     if n == 2:
